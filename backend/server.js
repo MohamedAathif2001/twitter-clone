dd89fa06d044740e5f9a128421b6cda7944745f2
aathif <mohamedaathif100001@gmail.com>
2024-06-23 19:50:46 +0530

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongodb.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
