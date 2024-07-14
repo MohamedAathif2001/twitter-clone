6271d1ff734da7b4eee8dd5c79833e80d156fc2f
aathif <mohamedaathif100001@gmail.com>
2024-07-14 12:12:32 +0530

    delete post added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,20 +5,20 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient(
-  {
-    defaultOptions: {
-      refetchOnWindowFocus: false,
-    }
-  }
-);
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
