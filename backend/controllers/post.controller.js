f484b3511b9161bede5d85c375526cfd73c1981b
aathif <mohamedaathif100001@gmail.com>
2024-07-14 13:52:00 +0530

    like comment added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -111,7 +111,9 @@ export const likeUnlikePost = async(req, res) => {
         if(userLikedPosts){
             await Post.updateOne({_id: postId}, {$pull: {likes: userId}})
             await User.updateOne({_id: userId}, {$pull: {likedPosts: postId}});
-            res.status(200).json({ message: "Post unliked successfully"});
+
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+            res.status(200).json(updatedLikes);
         }else{
             post.likes.push(userId);
             await User.updateOne({_id: userId}, {$push: {likedPosts: postId}});
@@ -123,7 +125,9 @@ export const likeUnlikePost = async(req, res) => {
                 type: "like",
             })
             await notification.save();
-            res.status(200).json({ message: "Post liked successfully"});
+
+            const updatedLikes = post.likes;
+            res.status(200).json(updatedLikes);
         }    
     }
     catch (error){
